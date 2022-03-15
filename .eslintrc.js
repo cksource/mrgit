44b2087e217fc44b8558facfbf22b04bd65be4a9
Przemyslaw Zan <p.zan@cksource.com>
2022-03-15 18:49:38 +0100

    Allowed the "license-header" ESLint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
 		node: true
 	},
 	rules: {
-		'no-console': 'off'
+		'no-console': 'off',
+		'ckeditor5-rules/license-header': [ 'error', { headerLines: [
+			'/**',
+			' * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.',
+			' * For licensing, see LICENSE.md.',
+			' */'
+		] } ]
 	}
 };
