8c0ff252eebbf7b28d771b376badb43b043d39c0
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-06-02 13:37:58 +0200

    Internal: Aligned release tools to the latest CKEditor 5 dev. Closes #159.

---
+++ scripts/preparepackages.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+const { Listr } = require( 'listr2' );
+const releaseTools = require( '@ckeditor/ckeditor5-dev-release-tools' );
+
+const latestVersion = releaseTools.getLastFromChangelog();
+const versionChangelog = releaseTools.getChangesForVersion( latestVersion );
+
+const tasks = new Listr( [
+	{
+		title: 'Verifying the repository.',
+		task: async () => {
+			const errors = await releaseTools.validateRepositoryToRelease( {
+				version: latestVersion,
+				changes: versionChangelog,
+				branch: 'master'
+			} );
+
+			if ( !errors.length ) {
+				return;
+			}
+
+			return Promise.reject( 'Aborted due to errors.\n' + errors.map( message => `* ${ message }` ).join( '\n' ) );
+		}
+	},
+	{
+		title: 'Updating the `#version` field.',
+		task: () => {
+			return releaseTools.updateVersions( {
+				version: latestVersion
+			} );
+		}
+	},
+	{
+		title: 'Creating the `mrgit2` package in the release directory.',
+		task: () => {
+			return releaseTools.prepareRepository( {
+				outputDirectory: 'release',
+				rootPackageJson: require( '../package.json' )
+			} );
+		}
+	},
+	{
+		title: 'Cleaning-up.',
+		task: () => {
+			return releaseTools.cleanUpPackages( {
+				packagesDirectory: 'release'
+			} );
+		}
+	},
+	{
+		title: 'Commit & tag.',
+		task: () => {
+			return releaseTools.commitAndTag( {
+				version: latestVersion,
+				files: [
+					'package.json'
+				]
+			} );
+		}
+	}
+] );
+
+tasks.run()
+	.catch( err => {
+		process.exitCode = 1;
+
+		console.log( '' );
+		console.error( err );
+	} );
