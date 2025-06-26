f12f33214267d733f5f2ec9f3744ea31c6add71b
Paweł Smyrek <p.smyrek@cksource.com>
2025-06-26 10:03:51 +0200

    Moved `ROOT_DIRECTORY` to `constants.mjs` util.

--- scripts/preparechangelog.mjs
+++ scripts/preparechangelog.mjs
@@ -3,14 +3,9 @@
  * For licensing, see LICENSE.md.
  */
 
-import upath from 'upath';
-import { fileURLToPath } from 'url';
 import { generateChangelogForSingleRepository } from '@ckeditor/ckeditor5-dev-changelog';
 import parseArguments from './utils/parsearguments.mjs';
-
-const __filename = fileURLToPath( import.meta.url );
-const __dirname = upath.dirname( __filename );
-const ROOT_DIRECTORY = upath.join( __dirname, '..' );
+import { ROOT_DIRECTORY } from './utils/constants.mjs';
 
 const cliOptions = parseArguments( process.argv.slice( 2 ) );
 
--- scripts/utils/constants.mjs
+++ scripts/utils/constants.mjs
@@ -3,4 +3,12 @@
  * For licensing, see LICENSE.md.
  */
 
+import upath from 'upath';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath( import.meta.url );
+const __dirname = upath.dirname( __filename );
+
+export const ROOT_DIRECTORY = upath.join( __dirname, '..', '..' );
+
 export const RELEASE_DIRECTORY = 'release';
